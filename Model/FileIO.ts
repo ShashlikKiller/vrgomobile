05b678a25dba5c5bc71eb2e4a24fc531a16223a9
Ditkarp <65854238+sainbaina@users.noreply.github.com>
2024-03-23 16:59:56 +0700

    Исправления реализации IDataProvider

---
+++ Model/FileIO.ts
@@ -0,0 +1,40 @@
+import * as fileSystem from 'fs';
+import { IDataProvider } from './interfaces/content-provider/IDataProvider';
+
+export class FileIO implements IDataProvider{
+    Set<T>(data: T, tag?: string): Promise<void> {
+        return writeFileAsync(tag, JSON.stringify(data));
+    }
+
+    Get<T>(tag: string): Promise<T> {
+        const filePath = tag || 'data.json';
+        return readFileAsync(filePath)
+            .then(data => JSON.parse(data) as T);
+    }
+}
+
+const readFileAsync = (filePath: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      fileSystem.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
+
+const writeFileAsync = (filePath?: string, content?: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        if (filePath != null && content != null){
+            fileSystem.writeFile(filePath, content, 'utf-8', (err) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve();
+                }
+              });
+        }
+  });
+}
