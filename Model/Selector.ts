8c13379a0ccb5a3193cddfbd9247443182242712
finik9808@gmail.com <finik9808@gmail.com>
2024-03-23 22:51:02 +0700

    Исправлены ошибки

--- Model/Selector.ts
+++ Model/Selector.ts
@@ -1,34 +1,42 @@
 import { Exercise } from "./Exercise";
-import { IExirciseSelctorBuilder, ISelector } from "./interfaces/IExerciseSelector";
+import { IExerciseSelctorBuilder, ISelector } from "./interfaces/IExerciseSelector";
 
-class Selector<T> implements ISelector<T> {
-    Select(array: T[], pathology: string, affectedRegions: string[]): T[] {
+class Selector implements ISelector<Exercise> {
+    private pathology: string;
+    private affectedRegions: string[];
+
+    constructor(pathology: string, affectedRegions: string[]) {
+        this.pathology = pathology;
+        this.affectedRegions = affectedRegions;
+    }
+
+    Select(array: Exercise[]): Exercise[] {
         return array.filter(item => {
             if (item instanceof Exercise) {
                 let exercise = item as Exercise;
-                return exercise.description.indexOf(pathology) !== -1 && affectedRegions.some(region => exercise.description.indexOf(region) !== -1);
+                return exercise.description.indexOf(this.pathology) !== -1 && this.affectedRegions.some(region => exercise.description.indexOf(region) !== -1);
             }
             return false;
         });
     }
 }
 
-class ExerciseSelectorBuilder implements IExirciseSelctorBuilder {
-    private pathology: string[] = [];
+class ExerciseSelectorBuilder implements IExerciseSelctorBuilder {
+    private pathology: string = "";
     private affectedRegion: string[] = [];
 
-    AddPathology(pathology: string): IExirciseSelctorBuilder {
-        this.pathology.push(pathology);
+    AddPathology(pathology: string): IExerciseSelctorBuilder {
+        this.pathology = pathology;
         return this;
     }
 
-    AddAffectedRegion(affectedRegion: string[]): IExirciseSelctorBuilder {
+    AddAffectedRegion(affectedRegion: string[]): IExerciseSelctorBuilder {
         this.affectedRegion.push(...affectedRegion);
         return this;
     }
 
     Build(): ISelector<Exercise> {
-        return new Selector<Exercise>();
+        return new Selector(this.pathology, this.affectedRegion);
     }
 }
 
@@ -38,6 +46,7 @@ let selector = selectorBuilder.AddPathology("Патология").AddAffectedReg
 let exercises = [
     new Exercise(1, "Exercise 1 with Патология and Рука", ["Step 1", "Step 2"], "image1.jpg"),
     new Exercise(2, "Exercise 2 with Нога", ["Step 1", "Step 2", "Step 3"], "image2.jpg"),
-    new Exercise(3, "Exercise 3 without Патология", ["Step 1"], "image3.jpg")]
-let selectedExercises = selector.Select(exercises, "Патология", ["Рука", "Нога"]);
+    new Exercise(3, "Exercise 3 without Патология", ["Step 1"], "image3.jpg")
+];
+let selectedExercises = selector.Select(exercises);
 console.log(selectedExercises);
--- Model/interfaces/IExerciseSelector.ts
+++ Model/interfaces/IExerciseSelector.ts
@@ -2,11 +2,11 @@ import { Exercise } from '../Exercise'
 import{IBuilder} from './IBuilder'
 
 export interface ISelector<T> {
-    Select(array:T[], pathology: string, affectedRegions: string[]):T[]
+    Select(array:T[]):T[]
 }
 export interface ISelectorBuilder<T> extends IBuilder<ISelector<T>>{}
 
-export interface IExirciseSelctorBuilder extends ISelectorBuilder<Exercise>{
-    AddPathology(pathology:string):IExirciseSelctorBuilder;
-    AddAffectedRegion(affectedRegion:string[]):IExirciseSelctorBuilder;
+export interface IExerciseSelctorBuilder extends ISelectorBuilder<Exercise>{
+    AddPathology(pathology:string):IExerciseSelctorBuilder;
+    AddAffectedRegion(affectedRegion:string[]):IExerciseSelctorBuilder;
 }
