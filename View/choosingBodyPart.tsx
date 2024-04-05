d109b4b0c8305b6fa79e852d8b2d03b1e445ac8d
maislaufen <dve14@tpu.ru>
2024-04-05 21:20:09 +0700

    Переделка нажатий

--- View/choosingBodyPart.tsx
+++ View/choosingBodyPart.tsx
@@ -97,7 +97,7 @@ export default function choosingBodyPart({navigation}: {navigation: any}) {
           <Text>К упражнениям</Text>
         </TouchableOpacity>
       </View>
-      <View style={{top: 0,  left: 0, right: 0, bottom: 0, aspectRatio: 1, flexDirection: 'column', flex: 1, position: 'absolute',  alignSelf: 'center', justifyContent: 'center' }}>
+      <View style={{top: 0,  left: '50%', right: 0, bottom: 0, aspectRatio: 1, flexDirection: 'column', flex: 1, width: 1, position: 'absolute',  alignSelf: 'center', justifyContent: 'center' }}>
         <View pointerEvents='none' style={{alignSelf: 'center', marginBottom: 5 }}>
         <Image resizeMode='contain' source={require('./bodyparts/head.png')}/>
         </View>        
@@ -128,7 +128,8 @@ export default function choosingBodyPart({navigation}: {navigation: any}) {
 const styles = StyleSheet.create({
   background: {
     backgroundColor: '#232323',
-    flex: 1
+    flex: 1,
+    justifyContent: 'center'
   },
   container: {
     flex: 1,
