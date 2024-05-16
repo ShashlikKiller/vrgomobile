ab1f1de9c7bae3b8aa6161d204b77728a4787a09
finik9808@gmail.com <finik9808@gmail.com>
2024-05-16 22:28:47 +0700

    Замена кнопки Далее на темную

--- source/screens/choosePat.tsx
+++ source/screens/choosePat.tsx
@@ -1,4 +1,4 @@
-import { NextButton } from '@components/buttonsComponent';
+import { NextButtonDark } from '@components/buttonsComponent';
 import DropdownComponent from '@components/patologyDropdownComponent';
 import { NavigationContext } from '@navigations/navigate';
 import React, { useContext, useState } from 'react';
@@ -24,7 +24,7 @@ export default function choosePat({ navigation }: { navigation: any }) {
           Начните вводить патологию или нарушение необходимое к физиотерапии
         </Text>
         <DropdownComponent onSelect={setSelectedPathology} dataProvider={dataProvider}/>
-        <NextButton action={loadScene}/>
+        <NextButtonDark action={loadScene}/>
       </View>
     </View>
   );
